acf9d4c1af4f249fccb947a02a7d27f084257e79
Gokhan Serce <mxpane@gmail.com>
2023-10-03 08:56:42 +0300

    Turtle Square added

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+dravingBoard = turtle.Screen()
+dravingBoard.bgcolor("red")
+dravingBoard.title("Python Square")
+
+lion = turtle.Turtle()
+lion.forward(100)
+lion.left(90)
+lion.forward(100)
+lion.left(90)
+lion.forward(100)
+lion.left(90)
+lion.forward(100)
+turtle.done()
